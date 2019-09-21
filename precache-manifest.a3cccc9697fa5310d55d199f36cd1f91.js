42d98a671a34997af230ac5e91f1ae96e6c34324
Matt Boisseau <boisseaumr@gmail.com>
2019-09-21 03:15:23 -0400

    Updates

---
+++ precache-manifest.a3cccc9697fa5310d55d199f36cd1f91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b03d6cbcbfc9f637bba4fa61df66f28f",
+    "url": "/dice-chains/index.html"
+  },
+  {
+    "revision": "405c02c1b373d37efb34",
+    "url": "/dice-chains/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e25e5f915c5fcba919bb",
+    "url": "/dice-chains/static/js/2.acac6566.chunk.js"
+  },
+  {
+    "revision": "405c02c1b373d37efb34",
+    "url": "/dice-chains/static/js/main.52d72911.chunk.js"
+  },
+  {
+    "revision": "288030889210dff1df89",
+    "url": "/dice-chains/static/js/runtime-main.00b28775.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/dice-chains/static/media/logo.25bf045c.svg"
+  }
+]);
